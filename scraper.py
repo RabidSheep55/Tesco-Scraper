7eb8c20e315d77e3b8d4e4f8f3ae8b725d83a2ce
RabidSheep55 <tharreaupierre@gmail.com>
2019-08-20 00:32:15 +0200

    rough fetching of products

--- scraper.py
+++ scraper.py
@@ -1,3 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import re
+from bs4 import BeautifulSoup
+from time import time
+
 '''
 Scrapes the tesco's deal page and saves the products in an ordered spreadsheet!
 Idea inspired by https://github.com/duplaja/shipt-sales-to-csv
@@ -8,13 +16,7 @@ NOTES:
 
 '''
 
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-import re
-
-from bs4 import BeautifulSoup
+batchSize = 100 # max 1000
 
 ################## Fetch the Data
 driver = webdriver.Chrome()
@@ -24,12 +26,29 @@ driver.get("https://www.tesco.com/groceries/en-GB/promotions/alloffers")
 
 # Wait until the number of items on sale is displayed
 wait = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "pagination__items-displayed")))
-itemsOnSale = re.search(r"(?<=of )\d+", driver.find_element_by_class_name("pagination__items-displayed").text).group()
-print("Total of", itemsOnSale, "items on sale right now")
+itemsOnSale = int(re.search(r"(?<=of )\d+", driver.find_element_by_class_name("pagination__items-displayed").text).group())
+print("There is a total of", itemsOnSale, "items on sale right now")
 
+# Now fetch all of them by batches
+itemsOnSale = 1000
+start = time()
+products = []
+for i in range(1, int(itemsOnSale/batchSize) + 1):
+    url = "https://www.tesco.com/groceries/en-GB/promotions/alloffers?page={}&count={}".format(str(i), str(batchSize))
+    driver.get(url)
+    wait = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "pagination-component")))
 
+    data = driver.find_element_by_css_selector("ul.product-list").get_attribute('innerHTML')
+    soup = BeautifulSoup(data, 'html.parser')
+    rawItemList = soup.find_all("li", recursive=False)
 
+    for item in rawItemList:
+        itemName = item.find("div", {"class": "product-details--content"}).find("a").text
+        products += [{"name": itemName}]
 
+end = time()
+print("Fetched items in", end-start, "ms")
+print(len(products))
 
 '''
 ################## Process the data into BeautifulSoup
