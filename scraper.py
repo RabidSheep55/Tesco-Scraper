05a824440cce98c288d760f84ef453219abee85d
RabidSheep55 <tharreaupierre@gmail.com>
2019-08-19 19:24:36 +0200

    more stuff

--- scraper.py
+++ scraper.py
@@ -1,17 +1,31 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from bs4 import BeautifulSoup
 
 # Scrapes the tesco's deal page and saves the products in an ordered spreadsheet!
 # Idea inspired by https://github.com/duplaja/shipt-sales-to-csv
-
 # Scraping is done with selenium and sifting of the page source is done with beautiful soup
-# Turns out there's no need for authentification
 
-# Fetch the Data
-print("Fetchin page data")
+################## Fetch the Data
+print("Fetching page data")
 
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
+# First find out how many items are on offer
+driver.get("https://www.tesco.com/groceries/en-GB/promotions/alloffers?page=1&count=100")
+
+# Wait untill the number of items on sale is displayed
+wait = WebDriverWait(driver, 30).until(EC.presence_of_element_located(By.CLASS_NAME, "pagination__items-displayed"))
+print(driver.find_element_by_class_name("pagination__items-displayed"))
+
+
+
+'''
+################## Process the data into BeautifulSoup
 # Load the data into BeautifulSoup
 soup = BeautifulSoup(data, 'html.parser')
 print("Raw page data loaded")
@@ -19,3 +33,4 @@ print("Raw page data loaded")
 # Manipulate the Data
 listContainer = soup.find("ul", {"class": "product-list"})
 rawItemList = listContainer.find_all("li")
+'''
