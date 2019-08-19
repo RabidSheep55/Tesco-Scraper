2dedd9b4ed650966460494230c649668c0c49eae
RabidSheep55 <tharreaupierre@gmail.com>
2019-08-19 20:47:02 +0200

    Succesfully fetches number of items on salse note: less articles shown when logged in... why?

--- scraper.py
+++ scraper.py
@@ -1,24 +1,33 @@
+'''
+Scrapes the tesco's deal page and saves the products in an ordered spreadsheet!
+Idea inspired by https://github.com/duplaja/shipt-sales-to-csv
+Scraping is done with selenium and sifting of the page source is done with beautiful soup
+
+NOTES:
+    - More items are shown to be on sale when not logged in... why?
+
+'''
+
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import re
 
 from bs4 import BeautifulSoup
 
-# Scrapes the tesco's deal page and saves the products in an ordered spreadsheet!
-# Idea inspired by https://github.com/duplaja/shipt-sales-to-csv
-# Scraping is done with selenium and sifting of the page source is done with beautiful soup
-
 ################## Fetch the Data
-print("Fetching page data")
 driver = webdriver.Chrome()
 
 # First find out how many items are on offer
-driver.get("https://www.tesco.com/groceries/en-GB/promotions/alloffers?page=1&count=100")
+driver.get("https://www.tesco.com/groceries/en-GB/promotions/alloffers")
 
-# Wait untill the number of items on sale is displayed
-wait = WebDriverWait(driver, 30).until(EC.presence_of_element_located(By.CLASS_NAME, "pagination__items-displayed"))
-print(driver.find_element_by_class_name("pagination__items-displayed"))
+# Wait until the number of items on sale is displayed
+wait = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "pagination__items-displayed")))
+itemsOnSale = re.search(r"(?<=of )\d+", driver.find_element_by_class_name("pagination__items-displayed").text).group()
+print("Total of", itemsOnSale, "items on sale right now")
+
+
 
 
 
