282200e7e123af413a1a0c0db6b451f04cec6567
RabidSheep55 <tharreaupierre@gmail.com>
2019-08-19 17:04:16 +0200

    desc

--- scraper.py
+++ scraper.py
@@ -4,4 +4,4 @@ from selenium.webdriver.common.keys import Keys
 # Scrapes the tesco's deal page and saves the products in an ordered spreadsheet!
 # Idea inspired by https://github.com/duplaja/shipt-sales-to-csv
 
-# Scraping is done with selenium
+# Scraping is done with selenium and sifting of the page source is done with beautiful soup
